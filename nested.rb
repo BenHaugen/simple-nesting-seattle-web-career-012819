63367c5cac477e68ffae3d50b2de502b92608ecb
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-24 15:46:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 
 	programmer_hash = 
  		{
@@ -81,7 +80,7 @@ programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
     
 end
 
